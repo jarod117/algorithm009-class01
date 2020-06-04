4e77557a5c06b5083c5b01d2961e7e1f4f58210a
jarod117@outlook.com <jarod>
2020-06-04 21:25:05 +0800

    Week_01 homework

---
+++ Week_01/moveZeroes.py
@@ -0,0 +1,36 @@
+﻿class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+#1. 数组
+# '''
+# 统计零元素个数，然后将非零元素放到数组的前面
+# '''
+        # count = 0
+        # new_nums = []
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         count += 1
+        # print(count)
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         print("i value", i, "num value: ", nums[i])
+        #         new_nums.append(nums[i])
+        # j = 0
+        # while j < count:
+        #     new_nums.append(0)
+        #     j += 1
+        # print("new nums: ", new_nums)
+        # nums = new_nums
+        # return nums
+
+#2. 双指针
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
+        
+        return nums
---
+++ Week_01/twoSum.py
@@ -0,0 +1,51 @@
+﻿class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+#1. 遍历数组
+#   先将一个值存入一个变量
+#   然后将另外的值，存入另一个变量
+#   累加尝试，是否两数之和等于目标值
+
+        # lis = []
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if i != j:
+        #             print('num i: ', nums[i])
+        #             print('num j: ', nums[j])
+        #             if target == nums[i] + nums[j]:
+        #                 lis.append(i)
+        #                 lis.append(j)
+        #                 return lis
+
+
+        # lis = []
+        # for i in range(len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if target == nums[i] + nums[j]:
+        #             lis.append(i)
+        #             lis.append(j)
+        #             return lis
+
+#   用单层循环，判断target - nums[i]是否在数组里
+        # lis = []
+        # for i in range(len(nums) - 1):
+        #     temp = target - nums[i]
+        #     if temp in nums[i + 1:]:
+        #         j = nums.index(temp, i + 1)
+        #         lis.append(i)
+        #         lis.append(j)
+        #         return lis
+
+#2. 哈希
+#   将数组元素放到一个字典里
+#   将数组的元素存储为键值，下表存储为值
+
+        dict = {}
+
+        for i, num in enumerate(nums):
+            dict[num] = i
+
+        for i in range(len(nums)):
+            temp = target - nums[i]
+            if dict.get(temp) and i != dict.get(temp):
+                return i, dict.get(temp)
